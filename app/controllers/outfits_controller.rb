ccccca4dbc948b936c33f1ea7344155e627a998b
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-02-18 20:23:20 +0900

    almost finished outfit-posting system (except saving :tag column)

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -18,16 +18,19 @@ class OutfitsController < ApplicationController
   end
 
   def create
-    @tag = Tag.create(outfit_params[:tag])
-    @tag.save
     @outfit = Outfit.create(outfit_params)
     @outfit.save
+    @tag = Tag.create(outfit_params[:tag_attributes])
+    @tag.save
     binding.pry
     redirect_to outfits_path
   end
 
   private
-  def outfit_params
-    params.require(:outfit).permit(:name, :image, :user_id, :tag)
-  end
+    def outfit_params
+      params.require(:outfit).permit(:name, :image, tag_attributes:[:id, :tagname]).merge(user_id: current_user.id)
+    end
+    # def tag_params
+    #   params.require(:tag).permit(:id, :tagname)
+    # end
   end
