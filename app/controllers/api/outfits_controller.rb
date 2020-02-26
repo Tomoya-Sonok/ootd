48d84bb69bc320e724adcc8c360db2a678ec7837
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-02-26 21:17:06 +0900

    ,

--- app/controllers/api/outfits_controller.rb
+++ app/controllers/api/outfits_controller.rb
@@ -1,7 +1,7 @@
 class Api::OutfitsController < ApplicationController
   def index
-    @outfit = Outfit.find(Outfit.pluck(:id).sample)
-
+    @outfit = current_user.outfits.find(Outfit.pluck(:id).sample)
   end
-
 end
+
+# @outfits = Outfit.where(id: params[:tag_id], user_id: current_user.id).order(created_at: :desc)
