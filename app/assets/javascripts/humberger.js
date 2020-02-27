0e0b682bb8963a2f134cb5c45602a6cc4bd0b05e
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-02-27 18:29:51 +0900

    wrote some css codes

--- app/assets/javascripts/humberger.js
+++ app/assets/javascripts/humberger.js
@@ -1,10 +1,5 @@
 $(function(){
   $("#menu-btn").on('change', function(){
-    $('.header-dropdown').toggle('slow');
-    // $(this).off('click');
-    // $(this).on('click');
-    console.log('humberger.js is running!');
-    
-
+    $('.header-dropdown').toggle('fast');
   });
 });
