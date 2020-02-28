7fb09a750b232fca740f90719d91a8ace875d9ab
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-02-28 20:02:45 +0900

    ran bundle exec rake assets:precompile

---
+++ app/javascript/components/Decide.js
@@ -0,0 +1,16 @@
+// import React from "react"
+// import PropTypes from "prop-types"
+// class Decide extends React.Component {
+//   render () {
+//     return (
+//       <React.Fragment>
+//         Outfit: {this.props.outfit}
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// Decide.propTypes = {
+//   outfit: PropTypes.string
+// };
+// export default Decide;
