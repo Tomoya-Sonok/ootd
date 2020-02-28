906795bfb0bc5c85d5980185f5fd9605797b95fd
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-02-28 19:14:19 +0900

    some changes made for S3 deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,17 +35,18 @@ namespace :deploy do
     invoke 'unicorn:stop'
     invoke 'unicorn:start'
   end
-end
 
-# アップロード
-desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
+
+  # アップロード
+  desc 'upload master.key'
+    task :upload do
+      on roles(:app) do |host|
+        if test "[ ! -d #{shared_path}/config ]"
+          execute "mkdir -p #{shared_path}/config"
+        end
+        upload!('config/master.key', "#{shared_path}/config/master.key")
       end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
-  end
-# before :starting, 'deploy:upload'
-# after :finishing, 'deploy:cleanup'
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
