6a44c39f9886845ce2870cd91590f23ed29c695b
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-02-28 16:50:49 +0900

    edited carrierwave just for test

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,14 +39,13 @@ end
 
 # アップロード
 desc 'upload master.key'
-task :upload do
-  on roles(:app) do |host|
-    if test "[ ! -d #{shared_path}/config ]"
-      execute "mkdir -p #{shared_path}/config"
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
-    upload!('config/master.key', "#{shared_path}/config/master.key")
   end
-end
 before :starting, 'deploy:upload'
 after :finishing, 'deploy:cleanup'
-end
