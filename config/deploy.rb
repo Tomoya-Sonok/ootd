080fde9575babae27d9c68e868726b74bd98c8f5
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-05 00:37:31 +0900

    config for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,9 +8,6 @@ set :application, 'ootd'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Tomoya-Sonok/ootd.git'
 
-#credentials.yml.encではmasterkeyにする
-set :linked_files, %w{config/master.key}
-
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -19,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/OOTD.pem'] 
+                  keys: ['~/.ssh/deploy-test.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -28,24 +25,13 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    # unicornを完全にキルしてから再起動する
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
   end
-
-
-  # アップロード
-  desc 'upload master.key'
-    task :upload do
-      on roles(:app) do |host|
-        if test "[ ! -d #{shared_path}/config ]"
-          execute "mkdir -p #{shared_path}/config"
-        end
-        upload!('config/master.key', "#{shared_path}/config/master.key")
-      end
-    end
-  before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
